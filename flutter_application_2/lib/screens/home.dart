e8640a72f06b5dba206c46ede7a40a04ce5c6a11
Shradha1824 <shradhabj1824@gmail.com>
2021-07-02 14:35:39 -1200

    MODIFIED: used Icon, padding, margin in flutter project

--- flutter_application_2/lib/screens/home.dart
+++ flutter_application_2/lib/screens/home.dart
@@ -7,6 +7,30 @@ class Home extends BaseScreen {
 }
 
 class HomeState extends BaseScreenState {
+  List<DropdownMenuItem<int>> listDrop = [];
+
+  void loadData() {
+    listDrop.add(new DropdownMenuItem(
+      child: new Text('Milimeters'),
+      value: 1,
+    ));
+
+    listDrop.add(new DropdownMenuItem(
+      child: new Text('Feet'),
+      value: 2,
+    ));
+
+    listDrop.add(new DropdownMenuItem(
+      child: new Text('Kilometers'),
+      value: 3,
+    ));
+
+    listDrop.add(new DropdownMenuItem(
+      child: new Text('Inches'),
+      value: 4,
+    ));
+  }
+
   Widget getAppBar() {
     return AppBar(
       backgroundColor: HexColor('#446DFF'),
@@ -19,116 +43,161 @@ class HomeState extends BaseScreenState {
   }
 
   Widget getBody(BuildContext context) {
-    var text = Text('1000',
-        textAlign: TextAlign.left,
-        style: TextStyle(
-            fontSize: 10.0, fontWeight: FontWeight.w700, color: Colors.black));
-    return Center(
-        child: Center(
+    loadData();
+    var valueChoose;
+    return new Center(
+        child: Container(
             child: SizedBox(
-                child: ListView(
-      children: <Widget>[
-        Container(
-            child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-              Text("CHOOSE TYPE",
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                      fontSize: 10.0,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.black)),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                      width: 120.0,
-                      height: 120.0,
-                      child: Card(
-                        child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Image.asset(
-                              'assets/images/scale.png',
-                              alignment: Alignment.center,
-                            )),
+                child: ListView(children: <Widget>[
+      Container(
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+            Text("CHOOSE TYPE",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    fontSize: 10.0,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black)),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  width: 130.0,
+                  height: 130.0,
+                  child: Card(
+                      margin: EdgeInsets.all(10.0),
+                      elevation: 8,
+                      child: Image.asset(
+                        'assets/images/scale.png',
+                        alignment: Alignment.center,
                       )),
-                  Container(
-                      width: 120.0,
-                      height: 120.0,
-                      child: Card(
-                          child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Image.asset(
-                          'assets/images/hot.png',
-                          alignment: Alignment.center,
-                        ),
-                      ))),
-                  Container(
-                      width: 120.0,
-                      height: 120.0,
-                      child: Card(
-                          child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Image.asset(
-                          'assets/images/beaker.png',
-                          alignment: Alignment.center,
-                        ),
-                      ))),
-                ],
-              )
-            ])),
-        Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("To",
-                  textAlign: TextAlign.left,
+                ),
+                Container(
+                  width: 130.0,
+                  height: 130.0,
+                  child: Card(
+                      margin: EdgeInsets.all(10.0),
+                      elevation: 8,
+                      child: Image.asset(
+                        'assets/images/hot.png',
+                        alignment: Alignment.center,
+                      )),
+                ),
+                Container(
+                  width: 130.0,
+                  height: 130.0,
+                  child: Card(
+                      margin: EdgeInsets.all(10.0),
+                      elevation: 8,
+                      child: Image.asset(
+                        'assets/images/beaker.png',
+                        alignment: Alignment.center,
+                      )),
+                ),
+              ],
+            )
+          ])),
+      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+        Expanded(
+            child: Container(
+                child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text("From ",
+                        style: TextStyle(
+                            fontSize: 10.0,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black))))),
+        Expanded(
+            child: Container(
+                child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text("To",
+                        style: TextStyle(
+                            fontSize: 10.0,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black)))))
+      ]),
+      Column(children: <Widget>[
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Container(
+              width: 150.0,
+              height: 50.0,
+              child: Text('1',
                   style: TextStyle(
                       fontSize: 10.0,
                       fontWeight: FontWeight.w700,
                       color: Colors.black)),
-              Text("From",
-                  textAlign: TextAlign.right,
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.all(0.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1),
+                borderRadius: BorderRadius.horizontal(),
+              )),
+          Container(
+              width: 150.0,
+              height: 50.0,
+              child: Text('1000',
                   style: TextStyle(
                       fontSize: 10.0,
                       fontWeight: FontWeight.w700,
                       color: Colors.black)),
-              Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                          width: 160.0,
-                          height: 60.0,
-                          child: Card(
-                            child: Padding(
-                                padding: const EdgeInsets.all(20.0),
-                                child: Text('1',
-                                    textAlign: TextAlign.left,
-                                    style: TextStyle(
-                                        fontSize: 10.0,
-                                        fontWeight: FontWeight.w700,
-                                        color: Colors.black))),
-                          )),
-                    ),
-                    Container(
-                      width: 160.0,
-                      height: 60.0,
-                      child: Card(
-                        child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Text('1000',
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                    fontSize: 10.0,
-                                    fontWeight: FontWeight.w700,
-                                    color: Colors.black))),
-                      ),
-                    ),
-                  ]),
-            ])
-      ],
-    ))));
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.all(0.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1),
+                borderRadius: BorderRadius.horizontal(),
+              ))
+        ]),
+        Column(children: <Widget>[
+          Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                    width: 150.0,
+                    padding: EdgeInsets.only(left: 16, right: 16),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1)),
+                    child: new DropdownButton(
+                        items: listDrop,
+                        hint: Text('Meter: '),
+                        dropdownColor: Colors.white,
+                        icon: Icon(Icons.arrow_drop_down),
+                        iconSize: 26,
+                        isExpanded: true,
+                        style: TextStyle(color: Colors.black, fontSize: 10),
+                        // value: valueChoose(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            valueChoose = newValue;
+                          });
+                        })),
+                Container(
+                    width: 150.0,
+                    padding: EdgeInsets.only(left: 16, right: 16),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1)),
+                    child: new DropdownButton(
+                        items: listDrop,
+                        hint: Text('Centimeter: '),
+                        dropdownColor: Colors.white,
+                        icon: Icon(Icons.arrow_drop_down),
+                        iconSize: 26,
+                        isExpanded: true,
+                        style: TextStyle(color: Colors.black, fontSize: 10),
+                        value: valueChoose,
+                        onChanged: (newValue) {
+                          setState(() {
+                            valueChoose = newValue;
+                          });
+                        }))
+              ]),
+        ]),
+      ])
+    ]))));
   }
 }
